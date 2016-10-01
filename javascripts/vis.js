80e798a74f4425feab2aff15ccba22a9f121d8b3
Avika Narula <an1520@nyu.edu>
2016-09-30 21:58:37 -0400

    haley's comet

--- javascripts/vis.js
+++ javascripts/vis.js
@@ -1,64 +1,79 @@
-	Papa.parse("/../data/comets.csv", {
-		complete: function(results) {
-			console.log("Finished:", results.data);
-		}
-	});
-	// var container;
-	// var camera, scene, renderer;
-	// var cube, plane;
-	// var targetRotation = 0;
-	// var targetRotationOnMouseDown = 0;
-	// var mouseX = 0;
-	// var mouseXOnMouseDown = 0;
-	// var windowHalfX = window.innerWidth / 2;
-	// var windowHalfY = window.innerHeight / 2;
-	// init();
-	// animate();
-	// function init() {
-	// 	container = document.getElementById( 'container' );
-	// 	document.body.appendChild( container );
-	// 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-	// 	camera.position.y = 150;
-	// 	camera.position.z = 700;
-	// 	scene = new THREE.Scene();
-	// 	// Cube
-	// 	var geometry = new THREE.BoxGeometry( 200, 200, 200 );
-	// 	for ( var i = 0; i < geometry.faces.length; i += 2 ) {
-	// 		var hex = Math.random() * 0xffffff;
-	// 		geometry.faces[ i ].color.setHex( hex );
-	// 		geometry.faces[ i + 1 ].color.setHex( hex );
-	// 	}
-	// 	var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
-	// 	cube = new THREE.Mesh( geometry, material );
-	// 	cube.position.y = 150;
-	// 	scene.add( cube );
-	// 	// Plane
-	// 	var geometry = new THREE.PlaneBufferGeometry( 200, 200 );
-	// 	geometry.rotateX( - Math.PI / 2 );
-	// 	var material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
-	// 	plane = new THREE.Mesh( geometry, material );
-	// 	scene.add( plane );
-	// 	renderer = new THREE.CanvasRenderer();
-	// 	renderer.setClearColor( 0xf0f0f0 );
-	// 	renderer.setPixelRatio( window.devicePixelRatio );
-	// 	renderer.setSize( window.innerWidth, window.innerHeight );
-	// 	container.appendChild( renderer.domElement );
-	// 	window.addEventListener( 'resize', onWindowResize, false );
-	// }
+Papa.parse("https://raw.githubusercontent.com/AvikaN/3d-comet-vis/master/data/comets.csv", {
+	download: true,
+	complete: function(results) {
+	//create scene after data loads
+		main(results); 
+	}
+});
 
-	// function onWindowResize() {
-	// 	windowHalfX = window.innerWidth / 2;
-	// 	windowHalfY = window.innerHeight / 2;
-	// 	camera.aspect = window.innerWidth / window.innerHeight;
-	// 	camera.updateProjectionMatrix();
-	// 	renderer.setSize( window.innerWidth, window.innerHeight );
-	// }
 
-	// function animate() {
-	// 	requestAnimationFrame( animate );
-	// 	render();
-	// }
-	// function render() {
-	// 	plane.rotation.y = cube.rotation.y += ( targetRotation - cube.rotation.y ) * 0.05;
-	// 	renderer.render( scene, camera );
-	// }
+function main(results){
+	var data = results.data;
+	if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+			var container;
+			var camera, controls, scene, renderer;
+			var cross;
+			init();
+			animate();
+			function init() {
+				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+				camera.position.z = 500;
+				initTrackBall();
+				// world
+				scene = new THREE.Scene();
+				var material = new THREE.LineBasicMaterial({
+			        color: 0xffffff, 
+			        linewidth : 2
+			    })
+				var geometry = new THREE.Geometry(); 
+				for(var i = 1; i < data.length - 2; i++){
+					geometry.vertices.push(new THREE.Vector3(data[i][0] * 5 , data[i][1] * 5 , data[i][2] * 2));
+				}
+
+				console.log(data);
+				
+			    var line = new THREE.Line(geometry, material);
+			    scene.add(line);
+				// renderer
+				renderer = new THREE.WebGLRenderer( { antialias: false } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container = document.getElementById( 'container' );
+				container.appendChild( renderer.domElement );
+				//
+				window.addEventListener( 'resize', onWindowResize, false );
+				//
+				render();
+			}
+			function onWindowResize() {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				controls.handleResize();
+				render();
+			}
+			function animate() {
+				requestAnimationFrame( animate );
+				controls.update();
+			}
+			function render() {
+				renderer.render( scene, camera );
+			}
+
+			function initTrackBall(){
+				controls = new THREE.TrackballControls( camera );
+							controls.rotateSpeed = 1.0;
+							controls.zoomSpeed = 1.2;
+							controls.panSpeed = 0.8;
+							controls.noZoom = false;
+							controls.noPan = false;
+							controls.staticMoving = true;
+							controls.dynamicDampingFactor = 0.3;
+							controls.keys = [ 65, 83, 68 ];
+							controls.addEventListener( 'change', render );
+			}
+
+}
+
+
+
